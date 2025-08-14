dcd7a19f0b89ba07e43952b6d573a804aa9d87c1
Huydemon1701 <phamtruongdanhuy.tcv3@gmail.com>
2025-08-14 15:18:36 +0700

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,12 @@
+package lec2_variable;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Sum int="+ (1 + 2));          
+        System.out.println("Sum double="+ (3.5 + 8));       
+        System.out.println("Div Double="+ (1.0 / 2)); //vì nếu để 1/2 thì java hiểu là int/int và trả về giá trị =0       
+        System.out.println("Sum double="+ (4.5 + 7));       
+        System.out.println("Result="+ (4 * 5 + 1));     
+        System.out.println("Result="+ (20 % 3 + 2 - 7)); 
+    }
+}
